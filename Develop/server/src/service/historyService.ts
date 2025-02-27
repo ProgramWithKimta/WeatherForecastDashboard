ac6bdfc279deda326fc0972bf82c3743f232030e
ProgramWithKimta <knguyen4268@gmail.com>
2025-02-26 21:57:14 -0600

    add codes to all the TODOs in historyService file

--- Develop/server/src/service/historyService.ts
+++ Develop/server/src/service/historyService.ts
@@ -1,17 +1,52 @@
 // TODO: Define a City class with name and id properties
+class City {
+  id: number;
+  name: string;
+
+  constructor(id: number, name: string) {
+    this.id = id;
+    this.name = name;
+  }
+};
 
 // TODO: Complete the HistoryService class
 class HistoryService {
   // TODO: Define a read method that reads from the searchHistory.json file
-  // private async read() {}
+  private async read() {
+    return await fs.readFile('/searchHistory.json', {
+      flag: 'a+',
+      encoding: 'utf8',
+    });
+  }
   // TODO: Define a write method that writes the updated cities array to the searchHistory.json file
-  // private async write(cities: City[]) {}
+  private async write(cities: City[]) {
+    return await fs.writeFile('searchHistory.json', JSON.stringify(cities, null, '\t'));
+  }
   // TODO: Define a getCities method that reads the cities from the searchHistory.json file and returns them as an array of City objects
-  // async getCities() {}
+  async getCities() {
+    return await this.read().then((cities) => {
+      let parsedCities: City[];
+    });
+  }
   // TODO Define an addCity method that adds a city to the searchHistory.json file
-  // async addCity(city: string) {}
+  async addCity(city: string) {
+    if (!city) {
+      throw new Error('city cannot be blank');
+    }
+    const newCity: City = {
+      id: id,
+      name: name,
+    };
+    return await this.getCities()
+    .then((cities) => {
+      return [...cities, newCity];
+    })
+    .then((updatedCities) => this.write(updatedCities))
+    .then(() => newCity);
+    }
+  }
   // * BONUS TODO: Define a removeCity method that removes a city from the searchHistory.json file
   // async removeCity(id: string) {}
-}
+
 
 export default new HistoryService();
